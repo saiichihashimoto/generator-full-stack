ae5ffc8d0b6ecfd5dc1e919a470dfa455bcc04cd
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-09-02 18:58:33 -0700

    Splitting out package from app

--- src/app/index.js
+++ src/app/index.js
@@ -1,25 +1,7 @@
 import { Base } from 'yeoman-generator';
 
-export default class KoggGenerator extends Base {
-	prompting() {
-		this.log();
-		this.log('Project Details');
-		this.log('---------------------------------');
-		return this.prompt([
-			{
-				message: 'Name',
-				name:    'name',
-				type:    'input',
-			},
-			{
-				message: 'Description',
-				name:    'description',
-				type:    'input',
-			},
-		]).then((answers) => this.answers = answers);
-	}
-	writing() {
-		// QUESTION Does this go here or in configuring?
-		this.fs.copyTpl(this.templatePath('package.json'), this.destinationPath('package.json'), this.answers);
+export default class AppGenerator extends Base {
+	initializing() {
+		this.composeWith('full-stack:package');
 	}
 }
---
+++ src/package/index.js
@@ -0,0 +1,37 @@
+import { Base } from 'yeoman-generator';
+
+export default class PackageGenerator extends Base {
+	prompting() {
+		this.log();
+		this.log('Package Details');
+		this.log('---------------------------------');
+		return this.prompt([
+			{
+				message: 'Name',
+				name:    'name',
+				type:    'input',
+			},
+			{
+				message: 'Description',
+				name:    'description',
+				type:    'input',
+			},
+			{
+				message: 'Node Version?',
+				name:    'node',
+				type:    'input',
+				default: '6.1.0',
+			},
+			{
+				message: 'NPM Version?',
+				name:    'npm',
+				type:    'input',
+				default: '3.9.0',
+			},
+		]).then((answers) => this.answers = answers);
+	}
+	writing() {
+		// QUESTION Does this go here or in configuring?
+		this.fs.copyTpl(this.templatePath('package.json'), this.destinationPath('package.json'), this.answers);
+	}
+}
