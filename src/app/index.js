7c3700131b66c7e04c18dd63ddf18d619b953f69
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-09-02 18:20:47 -0700

    Writing initial package.json

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,25 @@
 import { Base } from 'yeoman-generator';
 
 export default class KoggGenerator extends Base {
+	prompting() {
+		this.log();
+		this.log('Project Details');
+		this.log('---------------------------------');
+		return this.prompt([
+			{
+				message: 'Name',
+				name:    'name',
+				type:    'input',
+			},
+			{
+				message: 'Description',
+				name:    'description',
+				type:    'input',
+			},
+		]).then((answers) => this.answers = answers);
+	}
+	writing() {
+		// QUESTION Does this go here or in configuring?
+		this.fs.copyTpl(this.templatePath('package.json'), this.destinationPath('package.json'), this.answers);
+	}
 }
