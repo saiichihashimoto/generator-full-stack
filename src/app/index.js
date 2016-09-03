9d577f6ad7f9dde25cc1df0d09b6ca9e3193dabf
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-09-03 12:38:39 -0700

    Reset

--- src/app/index.js
+++ src/app/index.js
@@ -3,7 +3,8 @@ import { Base } from 'yeoman-generator';
 export default class AppGenerator extends Base {
 	initializing() {
 		this.argument('name', { type: String, required: true });
-		this.composeWith('full-stack:package', { args: [this.name] });
-		this.composeWith('full-stack:webpack', { args: [this.name] });
+	}
+	foo() {
+		this.log('foo');
 	}
 }
