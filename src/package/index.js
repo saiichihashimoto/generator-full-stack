7a86d2b7986ed47c167a761099097c8013816a15
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-09-02 19:05:16 -0700

    Ask questions once we learn that we need them

--- src/package/index.js
+++ src/package/index.js
@@ -16,18 +16,6 @@ export default class PackageGenerator extends Base {
 				name:    'description',
 				type:    'input',
 			},
-			{
-				message: 'Node Version?',
-				name:    'node',
-				type:    'input',
-				default: '6.1.0',
-			},
-			{
-				message: 'NPM Version?',
-				name:    'npm',
-				type:    'input',
-				default: '3.9.0',
-			},
 		]).then((answers) => this.answers = answers);
 	}
 	configuring() {
