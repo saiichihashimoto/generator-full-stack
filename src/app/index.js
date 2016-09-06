b5b889f5b0d02f3582f2a1606206cbb3dd04996b
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-09-05 23:40:53 -0700

    fix(webpack): Files that can only be used in webpack have ".web" in them

--- src/app/index.js
+++ src/app/index.js
@@ -24,11 +24,11 @@ export default class AppGenerator extends Base {
 		this.fs.copy(this.templatePath('components/routes.js'), this.destinationPath('components/routes.js'));
 		this.fs.copy(this.templatePath('entities/foos/schema.normalizr.js'), this.destinationPath('entities/foos/schema.normalizr.js'));
 		this.fs.copy(this.templatePath('entities/foos/service.js'), this.destinationPath('entities/foos/service.js'));
-		this.fs.copy(this.templatePath('entities/schemas.normalizr.js'), this.destinationPath('entities/schemas.normalizr.js'));
+		this.fs.copy(this.templatePath('entities/schemas.normalizr.web.js'), this.destinationPath('entities/schemas.normalizr.web.js'));
 		this.fs.copy(this.templatePath('index.js'), this.destinationPath('index.js'));
 		this.fs.copy(this.templatePath('index.web.js'), this.destinationPath('index.web.js'));
-		this.fs.copy(this.templatePath('redux/actions.js'), this.destinationPath('redux/actions.js'));
-		this.fs.copy(this.templatePath('redux/createStore.js'), this.destinationPath('redux/createStore.js'));
+		this.fs.copy(this.templatePath('redux/actions.web.js'), this.destinationPath('redux/actions.web.js'));
+		this.fs.copy(this.templatePath('redux/createStore.web.js'), this.destinationPath('redux/createStore.web.js'));
 		this.fs.copy(this.templatePath('redux/entities.actions.js'), this.destinationPath('redux/entities.actions.js'));
 		this.fs.copy(this.templatePath('redux/entities.reducer.js'), this.destinationPath('redux/entities.reducer.js'));
 		this.fs.copy(this.templatePath('report/index.js'), this.destinationPath('report/index.js'));
