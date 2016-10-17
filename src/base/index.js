c3ffdadf87ab3bf58aa85b1c146c636d8c701656
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-10-16 17:05:29 -0700

    perf(cli): Spawned commands output directly to stdio

--- src/base/index.js
+++ src/base/index.js
@@ -12,29 +12,18 @@ export class BaseGenerator extends Base {
 	}
 	_spawn(command, args, opt) {
 		return new Promise((resolve, reject) => {
+			const commandAsString = command + ' "' + args.join('" "') + '"';
 			this.log();
-			this.log('$', command, '"' + args.join('" "') + '"');
+			this.log('$', commandAsString);
 			this.log();
-			const spawn = this.spawnCommand(command, args, Object.assign({ stdio: 'pipe' }, opt));
-			let error = '';
-			let value = '';
 
-			spawn.stderr.on('data', (data) => {
-				console.error(data.toString());
-				error += data.toString();
-			});
+			const process = this.spawnCommand(command, args, opt);
 
-			spawn.stdout.on('data', (data) => {
-				console.log(data.toString());
-				value += data.toString();
-			});
-
-			spawn.on('close', (code) => {
+			process.once('exit', (code, signal) => {
 				if (code) {
-					reject(new Error(error));
-				} else {
-					resolve(value);
+					return reject(new Error('Spawned command failed with ' + code + ' ' + signal + ': ' + commandAsString));
 				}
+				resolve();
 			});
 		});
 	}
