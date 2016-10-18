0843ad3d758ed5d7060aaa53ee4aeeee89494431
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-10-17 17:53:11 -0700

    feat(test): Disable or enable test coverage
    #35

--- src/test/index.js
+++ src/test/index.js
@@ -1,15 +1,28 @@
 import { BaseGenerator } from '../base';
 
 export default class TestGenerator extends BaseGenerator {
+	constructor(...args) {
+		super(...args);
+
+		this.option('coverage', { defaults: true, type: Boolean });
+	}
 	writing() {
 		this.fs.extendJSON(this.destinationPath('package.json'), this.fs.readJSON(this.templatePath('package.json')));
 		this.npmInstall(
 			[
-				'istanbul',
 				'mocha',
 				'npm-run-all',
 			],
 			{ saveDev: true }
 		);
+		if (this.options.coverage) {
+			this.fs.extendJSON(this.destinationPath('package.json'), this.fs.readJSON(this.templatePath('coverage/package.json')));
+			this.npmInstall(
+				[
+					'istanbul',
+				],
+				{ saveDev: true }
+			);
+		}
 	}
 }
