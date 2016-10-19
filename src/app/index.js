04cea7ffa1ed988b109862f6e9ecf56ad909a1f1
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-10-19 14:44:57 -0700

    feat(generators): CLI options enable/disable generators
    closes #41

--- src/app/index.js
+++ src/app/index.js
@@ -6,13 +6,28 @@ export default class AppGenerator extends BaseGenerator {
 
 		this.option('name', { type: String });
 		this.option('description', { type: String });
+		this.option('ci', { defaults: true, type: Boolean });
+		this.option('repo', { defaults: true, type: Boolean });
+		this.option('server', { defaults: true, type: Boolean });
+		this.option('style', { defaults: true, type: Boolean });
+		this.option('test', { defaults: true, type: Boolean });
 	}
 	initializing() {
-		this.composeWith('full-stack:server', { options: this._passableOptions('server') });
-		this.composeWith('full-stack:repo', { options: this._passableOptions('repo') });
-		this.composeWith('full-stack:ci', { options: this._passableOptions('ci') });
-		this.composeWith('full-stack:test', { options: this._passableOptions('test') });
-		this.composeWith('full-stack:style', { options: this._passableOptions('style') });
+		if (this.options.server) {
+			this.composeWith('full-stack:server', { options: this._passableOptions('server') });
+		}
+		if (this.options.repo) {
+			this.composeWith('full-stack:repo', { options: this._passableOptions('repo') });
+		}
+		if (this.options.ci) {
+			this.composeWith('full-stack:ci', { options: this._passableOptions('ci') });
+		}
+		if (this.options.test) {
+			this.composeWith('full-stack:test', { options: this._passableOptions('test') });
+		}
+		if (this.options.style) {
+			this.composeWith('full-stack:style', { options: this._passableOptions('style') });
+		}
 	}
 	configuring() {
 		this.fs.copyTpl(this.templatePath('package.json'), this.destinationPath('package.json'), this.options);
--- src/ci/index.js
+++ src/ci/index.js
@@ -1,8 +1,15 @@
 import { BaseGenerator } from '../base';
 
 export default class CIGenerator extends BaseGenerator {
+	constructor(...args) {
+		super(...args);
+
+		this.option('travis', { defaults: true, type: Boolean });
+	}
 	initializing() {
-		this.composeWith('full-stack:travis', { options: this._passableOptions('travis') });
+		if (this.options.travis) {
+			this.composeWith('full-stack:travis', { options: this._passableOptions('travis') });
+		}
 	}
 	writing() {
 		this.fs.extendJSON(this.destinationPath('package.json'), this.fs.readJSON(this.templatePath('package.json')));
